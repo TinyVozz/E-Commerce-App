823a6c5a62807b18f7ec14265eb7daf87e57ae18
TinyVozz <lehoanglonggtzt>
2026-02-02 19:11:33 +0700

    Chuyển từ TypeScript sang JavaScript

---
+++ client/src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './style/style.scss'
+import App from './App.jsx'
+import { Provider } from 'react-redux'
+import { store } from './redux/store.js'
+
+const rootElement = document.getElementById('root')
+if (!rootElement) {
+  throw new Error('Root element not found')
+}
+
+createRoot(rootElement).render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+)
