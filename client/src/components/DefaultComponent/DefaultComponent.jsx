e7a3720cf7be602e2ed81f3849cd72507b4c17c8
TinyVozz <lehoanglonggtzt>
2026-02-16 15:00:03 +0700

    Thêm chức năng Toggle Dark Mode

--- client/src/components/DefaultComponent/DefaultComponent.jsx
+++ client/src/components/DefaultComponent/DefaultComponent.jsx
@@ -1,11 +1,41 @@
 import HeaderComponent from '../HeaderComponent/HeaderComponent'
+import { Col, Row } from 'antd';
 
-const DefaultComponent = ({children}) => {
+// Dark Mode
+import { ConfigProvider, theme } from "antd";
+import { useEffect, useState } from "react";
+
+const DefaultComponent = ({ children }) => {
+    // Load theme ngay khi app start (không bị flash)
+    const [dark, setDark] = useState(() => {
+        return localStorage.getItem("theme") === "dark";
+    });
+
+    // Khi dark thay đổi → cập nhật body + lưu localStorage
+    useEffect(() => {
+        const currentTheme = dark ? "dark" : "light";
+        document.body.setAttribute("data-theme", currentTheme);
+        localStorage.setItem("theme", currentTheme);
+    }, [dark]);
+
     return (
-        <div>
-            <HeaderComponent />
-            {children}
-        </div>
+        <ConfigProvider
+            theme={{
+                algorithm: dark
+                    ? theme.darkAlgorithm
+                    : theme.defaultAlgorithm,
+            }}
+        >
+            <HeaderComponent dark={dark} setDark={setDark} />
+
+            <Row>
+                <Col span={5}>
+
+                </Col>
+                <Col span={14}>{children}</Col>
+                <Col span={5}></Col>
+            </Row>
+        </ConfigProvider>
     )
 }
 
--- client/src/components/HeaderComponent/HeaderComponent.jsx
+++ client/src/components/HeaderComponent/HeaderComponent.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
 
-const HeaderComponent = () => {
+import { Col, Row, Input } from 'antd';
+import './HeaderComponent.scss'
+const { Search } = Input;
+import { ShoppingCart, Heart, MoonStar, Sun } from 'lucide-react';
+import { useEffect } from 'react';
+
+const HeaderComponent = ({ dark, setDark }) => {
+
+  useEffect(() => {
+    const theme = dark ? "dark" : "light";
+    document.body.setAttribute("data-theme", theme);
+  }, [dark]);
+
   return (
-    <div>HeaderComponent</div>
+    <div>
+      <Row className='header-wrapper' gutter={[48, 0]} align="middle">
+        <Col flex="none">
+          <div className="d-flex align-items-center justify-content-between gap-5">
+            <h1 className='header-logo'>I-Shop</h1>
+            {/* <MoonStar /> */}
+            {dark
+              ? (<Sun onClick={() => setDark(false)} />)
+              : (<MoonStar onClick={() => setDark(true)} />
+              )}
+          </div>
+        </Col>
+        <Col flex="auto">
+          <Search className='header-input'
+            placeholder="Input search text"
+            allowClear
+            enterButton
+            size="large"
+          // onSearch={onSearch}
+          />
+        </Col>
+        <Col span={6}>
+          <div className="right d-flex align-items-center justify-content-between">
+            <h3>Đăng nhập / Đăng ký</h3>
+
+            <div className="d-flex gap-4 p-4">
+              <ShoppingCart />
+              <Heart />
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </div>
   )
 }
 
